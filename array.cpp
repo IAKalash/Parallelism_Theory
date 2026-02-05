4cffb45f97388145acf4edc6e92a488e4ccc0a02
IAKalash <152734199I@mail.ru>
2026-02-05 17:20:48 +0700

    Task 1

---
+++ array.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <math.h>
+#include <numeric>
+
+#ifndef TYPE
+#define TYPE double
+#endif
+
+int main() {
+    size_t n = 10000000;
+    std::vector <TYPE> arr(n);
+
+    const double step = M_PI * 2 / n;
+
+    for (size_t i = 0; i < n; i++) {
+        arr[i] = sin(i * step);
+    }
+
+    TYPE sum = 0;
+    for (auto a : arr) sum += a;
+    std::cout << sum << std::endl;
+
+    return 0;
+}
